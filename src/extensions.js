dde797a8f16b62cb1475d58c15a4e771dde11bb0
William Boga IV <bill.boga@ritterim.com>
2015-01-15 08:22:30 -0500

    Add new string extension to normalize value. Behavior will transform an input like ///path//to/uri// into /path/to/uri/.

--- src/extensions.js
+++ src/extensions.js
@@ -1,3 +1,12 @@
+/**
+ * Normalizes a string by replacing multiple-instances
+ * of parameter `s` with the singular-instance.
+ * ex. "///path//to/my//endpoint//".normalize('/') => "/path/to/my/endpoint/"
+ */
+String.prototype.normalize = function(s) {
+    return this.replace(new RegExp(s + "+", "gm"), s);
+};
+
 String.prototype.trimBoth = function(s) {
 	return this.replace(new RegExp("^" + s + "+|" + s + "+$", "gm"), "");
 }
