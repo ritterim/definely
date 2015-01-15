3c91300297baba01e303d5582a8d999e4f738889
William Boga IV <bill.boga@ritterim.com>
2015-01-15 08:24:50 -0500

    Add layout path and replace tabs with spaces.

--- src/index.js
+++ src/index.js
@@ -7,28 +7,30 @@ import Lazy from 'lazy.js'
 var server = new Hapi.Server()
 
 server.connection({
-	port: 3000
+    port: 3000
 })
 
 server.views({
     engines: {
         html: require('handlebars')
     },
-    path: Path.join(__dirname, './views')
+    path: Path.join(__dirname, './views'),
+    layoutPath: Path.join(__dirname, './views/layouts'),
+    layout: 'layout'
 });
 
 controllers(server)
 
 server.start(() => {
-	console.log('Server running at:', server.info.uri)
-	outputRoutes()
+    console.log('Server running at:', server.info.uri)
+    outputRoutes()
 })
 
 
 function outputRoutes() {
-	console.log('Available Routes:')
-	Lazy(server.table()[0].table)
-	.map(route => route.method + ' ' + route.path)
-	.sort()
-	.each(e=>console.log(e))
+    console.log('Available Routes:')
+    Lazy(server.table()[0].table)
+        .map(route => route.method + ' ' + route.path)
+        .sort()
+        .each(e=>console.log(e))
 }
