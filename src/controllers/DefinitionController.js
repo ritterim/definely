e957a974c8a945baf5f688db42226d783a3c803e
William Boga IV <bill.boga@ritterim.com>
2015-01-15 08:27:42 -0500

    Add simple and example workflow for querying terms. Current behavior has DB-logic inline with controllers. The expectation is to refactor this logic into a separate module once the project reaches its first milestone, which is a simple proof of concept.

--- src/controllers/DefinitionController.js
+++ src/controllers/DefinitionController.js
@@ -1,14 +1,35 @@
 import Controller from './Controller'
+import pg from 'pg'
 
 export default class DefinitionController extends Controller {
-	constructor(router)
-	{
-		super(router, '/definitions')
-		this.get('/', this.index)
-	}
-	
-	index(request, reply) {
-		reply.view('definitionList') 
-	}
-	
+    constructor(router)
+    {
+        super(router, '/definitions')
+        this.get('/{id}', this.show)
+    }
+
+    show(request, reply) {
+        var id = request.params.id;
+
+        var client = new pg.Client(process.env['DATABASE_URL']);
+
+        client.connect(function(err) {
+            if(err) {
+                return console.error('Could not connect to postgres', err);
+            }
+
+            client.query('select t.term, t.tags, d.definition from terms t inner join definitions d on t.id = d.termid where d.id = $1', [id] , function(err, result) {
+                if(err) {
+                    return console.error('Error running query', err);
+                }
+
+                client.end();
+
+                reply.view('definitionList', {
+                    title: 'Definition for ' + result.rows[0].term,
+                    definition: result.rows[0]
+                });
+            });
+        });
+    }
 }
