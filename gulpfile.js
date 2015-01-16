2540d10810183c6cffda71bc0ae385643451c439
Allen Brubaker <allenbrubaker@gmail.com>
2015-01-16 10:38:21 -0500

    added term model with types and type assertions. Unit tests verify functionality

--- gulpfile.js
+++ gulpfile.js
@@ -37,7 +37,7 @@ gulp.task('build', ['clean'], function () {
 
 	return gulp.src('src/**/*.js')
 		.pipe($.sourcemaps.init())
-		.pipe($.traceur({annotations:true}))
+		.pipe($.traceur({annotations:true, types:true, typeAssertions:true, typeAssertionModule:'../assert'}))
 		//        .pipe($.concat('definely.js'))
 		.pipe($.sourcemaps.write('.'))
 		.pipe(gulp.dest('build'))
