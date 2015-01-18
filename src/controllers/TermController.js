ff2cbd6cbaa40f99ab36fe82e1e6c5e271be17d7
William Boga IV <bill.boga@ritterim.com>
2015-01-17 22:18:31 -0500

    Update term controller to allow creation and editing/updating of terms. Put all term-related views into separate terms view directory.

--- src/controllers/TermController.js
+++ src/controllers/TermController.js
@@ -4,21 +4,64 @@ import Database from '../database'
 
 export default class TermController extends Controller {
     constructor(router) {
-        super(router, '/terms')
-        this.get('/', this.index)
+        super(router, '/terms');
+
+        this.get('/', this.index.bind(this));
+        this.get('{id}', this.edit.bind(this));
+        this.get('/new', this.new);
+        this.post('/new', this.create.bind(this));
+        this.put('/{id}', this.update.bind(this));
+
+        this.database = new Database(process.env['DATABASE_URL']);
+    }
+
+    create(request, reply) {
+        this.database.add(
+            request.payload.term,
+            request.payload.tags,
+            request.payload.definition,
+            function(id) {
+                reply.redirect('/terms/' + id);
+            });
+    }
+
+    edit(request, reply) {
+        this.database.find(request.params.id, function(term) {
+            reply.view('terms/edit', {
+                title: 'Edit term',
+                term: term
+            });
+        });
     }
 
     index(request, reply) {
         var searchTerm = request.url.query['search-term'];
         var title = searchTerm == null ? 'Terms' : 'Search results for: ' + searchTerm;
 
-        var database = new Database(process.env['DATABASE_URL']);
-
-        database.search(searchTerm, function(dbResponse) {
-            reply.view('termList', {
+        this.database.search(searchTerm, function(dbResponse) {
+            reply.view('terms/index', {
                 title: title,
                 terms: dbResponse.rows
             });
         });
+    }
+
+    new(request, reply) {
+        reply.view('terms/new', {
+            title: 'New term'
+        });
+    }
+
+    update(request, reply) {
+        var id = request.params.id;
+
+        this.database.update(
+            id,
+            request.payload.term,
+            request.payload.tags,
+            request.payload.definition,
+            function() {
+                reply.redirect('/terms/' + id);
+            });
     }
 }
