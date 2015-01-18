bfecda7f3e14e862e2dcc3e1707a6fabaa9b27d5
William Boga IV <bill.boga@ritterim.com>
2015-01-17 22:10:22 -0500

    Refactor database module. Previous behavior closed db-connection after one use rather than using built-in connection pooling. Previous behavior also caused issues for any controller which only initialized the module once during the controller's initialization.

--- src/database.js
+++ src/database.js
@@ -2,86 +2,82 @@ import pg from 'pg';
 
 export default class Database{
     constructor(connectionUri){
-        this.client = new pg.Client(connectionUri);
+        this.connectionUri = connectionUri;
     };
 
     add(term, tags, definition, callback) {
-        var self = this;
-
-        this.client.connect(function(err) {
+        pg.connect(this.connectionUri, function(err, client, done) {
             if(err) {
                 return console.error('Could not connect to postgres', err);
+                done(client);
             }
 
-            self.client.query('insert into terms (term, tags, definition) values ($1, $2, $3);', [ term, tags, definition ], function(err, result) {
+            client.query('insert into terms (term, tags, definition) values ($1, $2, $3) returning id;', [ term, tags, definition ], function(err, result) {
                 if(err) {
                     return console.error('Error running query', err);
+                    done(client);
                 }
 
-                self.client.end();
-
-                callback(result);
+                done();
+                callback(result.rows[0].id);
             });
         });
     };
 
     find(id, callback) {
-        var self = this;
-
-        this.client.connect(function(err) {
+        pg.connect(this.connectionUri, function(err, client, done) {
             if(err) {
                 return console.error('Could not connect to postgres', err);
+                done(client);
             }
 
-            self.client.query('select id, term, tags, definition from terms where id = $1;', [ id ],
+            client.query('select id, term, tags, definition from terms where id = $1;', [ id ],
                               function(err, result) {
                                   if(err) {
                                       return console.error('Error running query', err);
+                                      done(client);
                                   }
 
-                                  self.client.end();
-
-                                  callback(result);
+                                  done();
+                                  callback(result.rows[0]);
                               });
         });
     };
 
     search(searchTerm, callback) {
-        var self = this;
-
-        this.client.connect(function(err) {
+        pg.connect(this.connectionUri, function(err, client, done) {
             if(err) {
                 return console.error('Could not connect to postgres', err);
+                done(client);
             }
 
-            self.client.query('select id, term, tags from terms, to_tsquery($1) as query where weightedVector @@ query order by ts_rank_cd(weightedVector, query) desc;', [ searchTerm ],
+            client.query('select id, term, tags from terms, to_tsquery($1) as query where weightedVector @@ query order by ts_rank_cd(weightedVector, query) desc;', [ searchTerm ],
                               function(err, result) {
                                   if(err) {
                                       return console.error('Error running query', err);
+                                      done(client);
                                   }
 
-                                  self.client.end();
-
+                                  done();
                                   callback(result);
                               });
         });
     };
 
     update(id, term, tags, definition, callback) {
-        var self = this;
-
-        this.client.connect(function(err) {
+        pg.connect(this.connectionUri, function(err, client, done) {
             if(err) {
                 return console.error('Could not connect to postgres', err);
+                done(client);
             }
 
-            self.client.query('update terms set term = $1, tags = $2, definition = $3 where id = $4;', [ term, tags, definition, id ], function(err, result) {
+            client.query('update terms set term = $1, tags = $2, definition = $3 where id = $4;', [ term, tags, definition, id ], function(err, result) {
                 if(err) {
                     return console.error('Error running query', err);
+                    done(client);
                 }
 
-                self.client.end();
-
+                done();
                 callback(result);
             });
         });
