9f623967fcc0ccf0ca92d4f60cc717327051c23b
William Boga IV <bogaracing@hotmail.com>
2015-01-18 20:42:09 -0500

    Revert "Revert "[WIP] Refactor web client""

--- src/index.js
+++ src/index.js
@@ -3,13 +3,18 @@ import Path from 'path'
 import controllers from './controllers/index'
 import _ from './extensions'
 import Lazy from 'lazy.js'
+import Handlebars from 'handlebars';
 
+Handlebars.registerHelper("currentYear", function() {
+    return new Date().getFullYear();
+});
 
 spawnServer('App', 3000, controllers, {
     engines: {
-        html: require('handlebars')
+        hbs: Handlebars
     },
     path: Path.join(__dirname, './views'),
+    partialsPath: Path.join(__dirname, './views/partials'),
     layoutPath: Path.join(__dirname, './views/layouts'),
     layout: 'layout'
 })
@@ -25,15 +30,25 @@ function spawnServer(name, port, controllers, viewOptions) {
 	if (viewOptions)
 		server.views(viewOptions);
 
+    server.ext('onRequest', function (request, reply) {
+        var methodOverride = request.query['hapi_method'];
+
+        if (methodOverride) {
+            request.setMethod(methodOverride);
+        }
+
+        return reply.continue();
+    });
+
     controllers(server)
 
     server.start(() => {
         console.log(name + ' server running at:', server.info.uri)
-        outputRoutes()
+        outputRoutes(server)
     })
 }
 
-function outputRoutes() {
+function outputRoutes(server) {
     console.log('Available Routes:')
     Lazy(server.table()[0].table)
         .map(route => route.method + ' ' + route.path)
