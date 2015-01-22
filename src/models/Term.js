155561b2c0380664cacbb853e4150782658e8eb4
Allen Brubaker <allenbrubaker@gmail.com>
2015-01-22 16:11:32 -0500

    Removed demo methods from Term object

--- src/models/Term.js
+++ src/models/Term.js
@@ -1,39 +1,42 @@
-import {Get, Post, Put, Patch, Delete} from '../siren/attributes'
+import {
+    Get, Post, Put, Patch, Delete
+}
+from '../siren/attributes'
 
 @Get('terms/:id')
 export default class Term {
-    constructor(id=0,term='',definition='',tags=[]) {
+    constructor(id = 0, term = '', definition = '', tags = []) {
         this.id = Number(id)
         this.term = term
-        this.tags = tags
+        this.tags = tags = (typeof tags) == "string" ? tags.split(' ') : tags
         this.definition = definition
     }
 
-    get id() {return this.id_ = this.id_ || 0 }
-    set id(value:number) { this.id_ = value }
-
-    get term() { return this.term_ = this.term_ || '' }
-    set term(value:string) { this.term_ = value}
-
-    get tags() { return this.tags_ = this.tags_ || []}
-    set tags(value:Array<string>) { this.tags_ = value}
-
-    get definition() { return this.definition_ = this.definition_ || ''}
-    set definition(value:string) { this.definition_ = value}
-
-    @Post('postUrl')
-    postMethod() {
+    get id() {
+        return this.id_ = this.id_ || 0
+    }
+    set id(value: number) {
+        this.id_ = value
     }
 
-    @Put('putUrl')
-    putMethod(a) {
+    get term() {
+        return this.term_ = this.term_ || ''
+    }
+    set term(value: string) {
+        this.term_ = value
     }
 
-    @Patch('patchUrl')
-    patchMethod(b,c) {
+    get tags() {
+        return this.tags_ = this.tags_
+    }
+    set tags(value: Array<string>) {
+        this.tags_ = value
     }
 
-    @Delete('deleteUrl')
-    deleteMethod(d,e,f) {
+    get definition() {
+        return this.definition_ = this.definition_ || ''
+    }
+    set definition(value: string) {
+        this.definition_ = value
     }
 }
