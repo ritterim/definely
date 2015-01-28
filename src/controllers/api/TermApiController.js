5f7d8acf188eb90d78f77c13e715ec7c71bf333a
Allen Brubaker <allenbrubaker@gmail.com>
2015-01-28 11:07:45 -0500

    Promisified database

--- src/controllers/api/TermApiController.js
+++ src/controllers/api/TermApiController.js
@@ -17,7 +17,7 @@ default class TermApiController extends ApiController {
 
     index(request, reply) {
         var searchTerm = request.url.query['search-term'];
-        this.database.search(searchTerm, function (terms) {
+        this.database.search(searchTerm).then(terms => {
             var siren = super.siren(terms)
             reply(siren).type('application/vnd.siren+json')
         })
@@ -25,26 +25,25 @@ default class TermApiController extends ApiController {
 
     new(request, reply) {
         var term = new Term(request.payload.id, request.payload.term, request.payload.definition, request.payload.tags)
-        this.database.add(
-            term,
-            function (id) {
-                term.id = id
-                var siren = super.siren(term)
-                reply(siren).type('application/vnd.siren+json')
-            })
-    }
-
-    show(request, reply) {
-        this.database.find(request.params.id, function (term) {
+        this.database.add(term)
+        then(id => {
+            term.id = id
             var siren = super.siren(term)
             reply(siren).type('application/vnd.siren+json')
         })
     }
 
+    show(request, reply) {
+        this.database.find(request.params.id)
+            .then(term => {
+                var siren = super.siren(term)
+                reply(siren).type('application/vnd.siren+json')
+            })
+    }
+
     update(request, reply) {
         var id = request.params.id
         var term = new Term(id, request.payload.term, request.payload.definition, request.payload.tags)
-        this.database.update(
-            term, () => reply())
+        this.database.update(term).then(() => reply())
     }
 }
--- src/database.js
+++ src/database.js
@@ -3,8 +3,6 @@ import pg from 'pg';
 Promise.promisifyAll(pg)
 import Term from './models/Term'
 
-
-
 export
 default class Database {
     constructor(connectionUri) {
@@ -12,108 +10,67 @@ default class Database {
     }
 
     add(term) {
-        return this._connect(this.connectionUri).then(client =>
-            client.queryAsync('insert into terms (term, tags, definition) values ($1, $2, $3) returning id;', [term.term, term.tags.join(' '), term.definition])
-            .then(result => {
-                return result.rows[0].id
-            })
-        )
+        return Promise.using(this._connect(), client =>
+                client.queryAsync('insert into terms (term, tags, definition) values ($1, $2, $3) returning id;', [term.term, term.tags.join(' '), term.definition])
+                .then(result => result.rows[0].id))
+            .error(err => console.error('Error running query', err))
     }
 
     find(id) {
-        //        return this._connect(this.connectionUri).then(client => {
-
-        pg.connect(this.connectionUri, function (err, client, done) {
-            if (err) {
-                return console.error('Could not connect to postgres', err);
-                done(client);
-            }
-
-            client.query('select id, term, tags, definition from terms where id = $1;', [id],
-                function (err, result) {
-                    if (err) {
-                        return console.error('Error running query', err);
-                        done(client);
-                    }
-
-                    done();
+        return Promise.using(this._connect(), client =>
+                client.queryAsync('select id, term, tags, definition from terms where id = $1;', [id])
+                .then(result => {
                     var term = null
                     if (result.rows.length > 0) {
                         var row = result.rows[0]
                         term = new Term(row.id, row.term, row.definition, row.tags || undefined)
                     }
-                    callback(term);
-                });
-        });
-    };
+                    return term
+                }))
+            .error(err => console.error('Error running query', err))
+    }
 
-    search(searchTerm, callback) {
-        pg.connect(this.connectionUri, function (err, client, done) {
-            if (err) {
-                return console.error('Could not connect to postgres', err);
-                done(client);
-            }
+    search(searchTerm) {
+        return Promise.using(this._connect(), client => {
+                searchTerm = (searchTerm || '').trim().replace(/\s+/g, ' | ');
 
-            searchTerm = (searchTerm || '').trim().replace(/\s+/g, ' | ');
-
-            if (!searchTerm) {
-                client.query('select id, term, tags, definition from terms', function (err, result) {
-
-                    var terms = result.rows.map(row => new Term(row.id, row.term, row.definition, row.tags || undefined))
-                    callback(terms)
-                })
-            } else {
-                client.query('select id, term, tags, definition, rank, ts_headline(definition, query) as highlight_definition from terms, to_tsquery($1) as query, ts_rank_cd(weightedVector, query) as rank where weightedVector @@ query order by rank desc;', [searchTerm],
-                    function (err, result) {
-                        if (err) {
-                            return console.error('Error running query', err);
-                            done(client);
-                        }
-
-                        done();
-                        var terms = result.rows.map(row => {
-                            var term = new Term(row.id, row.term, row.definition, row.tags || undefined);
-
+                if (!searchTerm)
+                    return client.queryAsync('select id, term, tags, definition from terms')
+                        .then(result => result.rows.map(row => new Term(row.id, row.term, row.definition, row.tags || undefined)))
+                else
+                    return client.queryAsync('select id, term, tags, definition, rank, ts_headline(definition, query) as highlight_definition from terms, to_tsquery($1) as query, ts_rank_cd(weightedVector, query) as rank where weightedVector @@ query order by rank desc;', [searchTerm])
+                        .then(result =>
+                            result.rows.map(row => {
+                                var term = new Term(row.id, row.term, row.definition, row.tags || undefined)
+                                
                             /**
                              * This property is dynamically added since it's specific
                              * to this function and not part of the model.
                              */
-                            term.rank = row.rank;
-                            term.highlightDefinition = row.highlight_definition;
-
-                            return term;
-                        });
-
-                        callback(terms);
-                    })
-            }
-        })
+                                term.rank = row.rank
+                                term.highlightDefinition = row.highlight_definition
+                                return term
+                            })
+                        )
+            })
+            .error(err => console.error('Error running query', err))
     }
 
-    update(term, callback) {
-        pg.connect(this.connectionUri, function (err, client, done) {
-            if (err) {
-                return console.error('Could not connect to postgres', err);
-                done(client);
-            }
-            client.query('update terms set term = $1, tags = $2, definition = $3 where id = $4;', [term.term, term.tags.join(' '), term.definition, term.id], function (err, result) {
-                if (err) {
-                    return console.error('Error running query', err);
-                    done(client);
-                }
-                done();
-                callback();
-            });
-        });
-    };
+    update(term) {
+        return Promise.using(this._connect(), client =>
+                client.queryAsync('update terms set term = $1, tags = $2, definition = $3 where id = $4;', [term.term, term.tags.join(' '), term.definition, term.id]))
+            .error(err => console.error('Error running query', err))
+    }
 
-    _connection(connectionString) {
+    _connect() {
         var close;
-        return pg.connectAsync(connectionString).spread(function (client, done) {
-            close = done;
-            return client;
-        }).disposer(function (client) {
-            if (close) close(client);
-        }).error(err => console.log('Could not connect to postgres', err))
+        return pg.connectAsync(this.connectionUri).spread(function (client, done) {
+                close = done
+                return client
+            })
+            .error(err => console.log('Could not connect to postgres', err))
+            .disposer(function (client) {
+                if (close) close(client)
+            })
     }
 }
